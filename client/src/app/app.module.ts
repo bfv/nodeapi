9e31fd30e85797c52182f587042ed60d57699756
bfv <bronco@oostermeyer.nl>
2017-08-10 10:36:02 +0200

    setup routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRoutingModule,
   ],
   imports: [
     BrowserModule
